98dcc47def45e04fed7843051cd733462237cd9b
Znjis <ilyes2011@live.ca>
2024-02-17 22:03:21 -0500

    fix: pipeline was failling because of linter

--- src/main/java/ca/ulaval/glo2003/models/RestaurantResponse.java
+++ src/main/java/ca/ulaval/glo2003/models/RestaurantResponse.java
@@ -1,11 +1,9 @@
 package ca.ulaval.glo2003.models;
 
-import ca.ulaval.glo2003.domain.reservation.Reservation;
 import ca.ulaval.glo2003.domain.utils.Hours;
 import ca.ulaval.glo2003.domain.restaurant.ReservationConfiguration;
 import ca.ulaval.glo2003.domain.restaurant.Restaurant;
 
-import java.util.Map;
 
 public class RestaurantResponse {
     public String id;
